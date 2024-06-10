984d1200303076fd9f41e978b141cbd81ee80666
alipala.ist@gmail.com <alipala.ist@gmail.com>
2024-06-10 12:58:14 +0200

    Added BaseTest

---
+++ src/test/java/org/testproject/BaseTest.java
@@ -0,0 +1,23 @@
+package org.testproject;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+
+public abstract class BaseTest {
+
+    @BeforeAll
+    public static void setUp() {
+        RestAssured.baseURI = "https://www.rijksmuseum.nl/api/en";
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    }
+
+    protected void healthCheck() {
+        RestAssured.
+                given().
+                when().
+                get("/health").
+                then().
+                statusCode(200);
+
+    }
+}
